b74c211ab01a5b7afd5e145b022fdc102377c67e
VincentDS98 <disanto.vincenzo98@gmail.com>
2023-11-30 15:22:34 +0100

    Java script

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+let nome = prompt ('Dimmi il tuo Nome');
+document.writeln(nome)
+
+let cognome = prompt ('Dimmi il tuo Cognome');
+document.writeln(cognome)
+
+let colore = prompt ('Dimmi il tuo colore preferito');
+document.writeln(colore)
+
+document.writeln(114)
+
